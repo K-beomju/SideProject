64bd2a2aa19bec5a86d4b9b1ba9cd8e84c938ff7
Kimbeomju <kbj52264291@gmail.com>
2022-07-05 00:00:55 +0900

    기초 프로젝트 세팅 완료

---
+++ SideProject/Assets/01.Scripts/Manager/SoundManager.cs
@@ -0,0 +1,239 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public float MasterVoulme => masterVoulme;
+    public float BGMVolume => bgmVolume * MasterVoulme;
+    public float FxVoulme => fxVoulme * MasterVoulme;
+
+    public float masterVoulme = 1.0f;
+    public float bgmVolume = 1.0f;
+    public float fxVoulme = 1.0f;
+    public float pitch = 1.0f;
+
+    private AudioSource bgmAudioSource;
+    private List<AudioSource> fxAudioSourceList = new List<AudioSource>();
+
+    private Dictionary<string, AudioClip> bgmSoundDic = new Dictionary<string, AudioClip>();
+    private Dictionary<string, AudioClip> fxSoundDic = new Dictionary<string, AudioClip>();
+
+    public static bool SoundOff = false;
+
+
+    private static SoundManager instance;
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<SoundManager>(); 
+                if (instance == null) 
+                {
+                    instance = new GameObject(typeof(SoundManager).ToString()).AddComponent<SoundManager>(); 
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as SoundManager;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this);
+
+        foreach (var audioClip in Resources.LoadAll<AudioClip>("Sound/BGM")) 
+        {
+            bgmSoundDic.Add(audioClip.name, audioClip);
+        }
+    }
+
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = default;
+        }
+    }
+
+    private AudioClip GetBGMSound(string name)
+    {
+        AudioClip result;
+        if (!bgmSoundDic.TryGetValue(name, out result))
+        {
+            Debug.LogWarning(name + "Not Found");
+        }
+        return result;
+    }
+
+    private AudioClip GetFxSound(string name)
+    {
+        AudioClip result;
+        if (!fxSoundDic.TryGetValue(name, out result))
+        {
+            result = Resources.Load<AudioClip>("Sound/FX/" + name);
+            if (result == null)
+            {
+                Debug.LogWarning(name + "Not Found");
+                return null;
+            }
+            fxSoundDic.Add(name, result);
+        }
+        return result;
+    }
+
+    private AudioSource MakeAudioSourceObject(string name)
+    {
+        GameObject audioObject = new GameObject();
+        audioObject.name = name;
+        audioObject.transform.SetParent(gameObject.transform);
+
+        return audioObject.AddComponent<AudioSource>();
+    }
+
+    private void SetAudioSource(AudioSource audioSource, AudioClip audioClip, bool isLoop, float volume, bool isMute = false)
+    {
+        audioSource.clip = audioClip;
+        audioSource.loop = isLoop;
+        audioSource.volume = volume;
+        audioSource.mute = isMute;
+    }
+
+    public void AdjustMasterVolume(float newVolume)
+    {
+        masterVoulme = newVolume;
+        AdjustBGMVolume(bgmVolume);
+        AdjustFxVoulme(fxVoulme);
+    }
+
+    public void AdjustBGMVolume(float newVolume)
+    {
+        bgmVolume = newVolume;
+        if (bgmAudioSource != null)
+        {
+            bgmAudioSource.volume = BGMVolume;
+        }
+    }
+
+    public void AdjustFxVoulme(float newVolume)
+    {
+        fxVoulme = newVolume;
+        foreach (var fxAudioSource in fxAudioSourceList)
+        {
+            if (fxAudioSource != null)
+            {
+                fxAudioSource.volume = FxVoulme;
+            }
+        }
+    }
+
+    public void AdjustSoundPitch(float pitch)
+    {
+        this.pitch = pitch;
+        foreach (var item in fxAudioSourceList)
+        {
+            if (item)
+            {
+                item.DOPitch(pitch, 0.5f).SetUpdate(true);
+            }
+        }
+
+        if (bgmAudioSource)
+        {
+            bgmAudioSource.DOPitch(pitch, 0.5f).SetUpdate(true);
+        }
+    }
+
+    public void PlayBGMSound(string name, float duration = 2)
+    {
+        if (SoundOff) return;
+
+        if (bgmAudioSource == null)
+        {
+            bgmAudioSource = MakeAudioSourceObject("BGMObject");
+        }
+
+        if (bgmAudioSource.isPlaying)
+        {
+            StartCoroutine(ChangeBGM(BGMVolume, name, duration));
+        }
+        else
+        {
+            SetAudioSource(bgmAudioSource, GetBGMSound(name), true, BGMVolume, false);
+            bgmAudioSource.Play();
+        }
+
+    }
+
+
+    public IEnumerator ChangeBGM(float volume, string name, float duration)
+    {
+        DOTween.To(() => bgmAudioSource.volume, x => bgmAudioSource.volume = x, 0, duration)
+            .OnComplete(() =>
+            {
+                bgmAudioSource.volume = volume * MasterVoulme;
+                SetAudioSource(bgmAudioSource, GetBGMSound(name), true, BGMVolume, false);
+                bgmAudioSource.Play();
+            });
+        yield return null;
+    }
+
+    public void PlayFXSound(string name)
+    {
+        if (SoundOff) return;
+
+        foreach (var fxAudioSource in fxAudioSourceList)
+        {
+            if (!fxAudioSource.isPlaying)
+            {
+                SetAudioSource(fxAudioSource, GetFxSound(name), false, FxVoulme, false);
+                fxAudioSource.Play();
+                return;
+            }
+        }
+
+        fxAudioSourceList.Add(MakeAudioSourceObject("FxObject"));
+        PlayFXSound(name);
+    }
+
+    public void StopBGMSound(float duration = 2)
+    {
+        DOTween.To(() => bgmAudioSource.volume, x => bgmAudioSource.volume = x, 0, duration)
+            .OnComplete(() =>
+            {
+                bgmAudioSource.Stop();
+            });
+    }
+
+    public void OffSound()
+    {
+        if (bgmAudioSource == null) return;
+
+        bgmAudioSource.volume = 0;
+        for (int i = 0; i < fxAudioSourceList.Count; i++)
+        {
+            fxAudioSourceList[i].volume = 0;
+        }
+
+
+    }
+
+   
+
+
+
+
+}
---
+++ SideProject/Assets/01.Scripts/Save/MGSaveData.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public  class MGSaveData : MonoBehaviour
+{
+    private static MGSaveData _instance;
+    public static MGSaveData instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(MGSaveData)) as MGSaveData;
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.hideFlags = HideFlags.HideAndDontSave;
+                    _instance = obj.AddComponent<MGSaveData>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private string fileName = "svDt.dat";
+
+    private SaveData saveData;
+
+    private static BinaryFormatter _binaryFormatter;
+    private static FileStream _fileStream;
+
+    public void generate()
+    {
+        Debug.LogWarning("-------------------");
+        Load();
+
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+            Destroy(this.gameObject);
+
+
+    }
+
+    private void Start()
+    {
+        generate();
+    }
+
+    [ContextMenu("SAVE")]
+    public void Save()
+    {
+        Debug.LogWarning("데이터를 저장합니다.");
+        _binaryFormatter = new BinaryFormatter();
+        _fileStream = File.Create(getFilePath(fileName));
+
+        _binaryFormatter.Serialize(_fileStream, saveData);
+
+        _fileStream.Close();
+    }
+
+    [ContextMenu("LOAD")]
+    public void Load()
+    {
+        saveData = new SaveData();
+        int classVer = saveData.version;
+
+        try
+        {
+            if (File.Exists(getFilePath(fileName)))
+            {
+                _binaryFormatter = new BinaryFormatter();
+                _fileStream = File.Open(getFilePath(fileName), FileMode.Open);
+
+                saveData = (SaveData)_binaryFormatter.Deserialize(_fileStream);
+
+                int fileVer = saveData.version;
+
+                // 세이브 파일 버전 체크
+                if (classVer != fileVer)
+                {
+                    // 바뀐 버전에 맞는 처리를 해줘야 함
+                    Debug.LogFormat("Savefile version : class:{0},file:{1}", classVer, fileVer);
+                }
+
+
+
+                _fileStream.Close();
+            }
+            else
+            {
+                // Null File => Create File 
+                Debug.LogWarning("Create New SaveFile!");
+                Save();
+            }
+
+            Debug.LogWarning("데이터를 로드합니다.");
+
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError("Error Loading Save " + e);
+            File.Delete(getFilePath(fileName));
+            Save();
+        }
+    }
+
+    [ContextMenu("Delete")]
+    public  void Delete()
+    {
+        if(File.Exists(getFilePath(fileName)))
+        {
+            try
+            {
+                File.Delete(getFilePath(fileName));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+        }
+
+    }
+
+    string getFilePath(string fileName)
+    {
+        return Application.persistentDataPath + "/" + fileName;
+    }
+
+    public SaveData GetSaveData()
+    {
+        return saveData;
+    }
+
+    public UserInfoData GetUserInfoData()
+    {
+        return saveData._myUserInfoDt;
+    }
+
+
+
+
+}
